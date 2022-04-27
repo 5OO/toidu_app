a608e22e1fd902362df597600d18b8bee27ae0d9
500 <git.hub@myrk.eu>
2022-04-27 13:23:40 +0300

    SQL file update - added db.sisend

---
+++ db/db_sisend_väärtustatud.sql
@@ -0,0 +1,8 @@
+INSERT INTO toidu_app.role (id, name) VALUES (1, 'admin');
+INSERT INTO toidu_app.role (id, name) VALUES (2, 'user');
+
+INSERT INTO toidu_app.users (id, username, password) VALUES (1, 'karli', 'karli123');
+INSERT INTO toidu_app.users (id, username, password) VALUES (2, 'henri', 'henri123');
+INSERT INTO toidu_app.users (id, username, password) VALUES (3, 'jaan', 'jaan123');
+INSERT INTO toidu_app.users (id, username, password) VALUES (4, 'rain', 'rain123')
+
