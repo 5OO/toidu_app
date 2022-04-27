16a957a7688689c4424252b33cbb4ff2239f31ac
500 <git.hub@myrk.eu>
2022-04-27 11:04:59 +0300

    Initial commit

---
+++ src/main/java/com/vali_it/toidu_app/ToiduAppApplication.java
@@ -0,0 +1,13 @@
+package com.vali_it.toidu_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ToiduAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ToiduAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/vali_it/toidu_app/ToiduAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vali_it.toidu_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ToiduAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
