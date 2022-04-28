f287be72b6d0c061fa418f23e9448dbacf177019
KarliEST <karlikullasepp@gmail.com>
2022-04-28 16:25:12 +0300

    Ingredient controller ja service

---
+++ src/main/java/com/vali_it/toidu_app/domain/ingredient/ingredient/IngredientController.java
@@ -0,0 +1,28 @@
+package com.vali_it.toidu_app.domain.ingredient.ingredient;
+
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.annotation.Resource;
+import java.util.List;
+
+@RestController
+@RequestMapping("/ingredient")
+public class IngredientController {
+
+    @Resource
+    private IngredientService ingredientService;
+
+    @GetMapping("/all")
+    public List<IngredientDto> getAllIngredients() {
+        return ingredientService.getAllIngredients();
+    }
+
+    @GetMapping("/find")
+    public List<IngredientDto> findIngredientByName(@RequestParam Integer id) {
+        return ingredientService.findIngredientById(id);
+    }
+}
