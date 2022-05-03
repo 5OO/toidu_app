5bec1945b25cfaad2fc14284635612538179231c
500 <git.hub@myrk.eu>
2022-05-03 14:31:43 +0300

    JAAN tegin uue retspti lisamise!!!

---
+++ src/main/java/com/vali_it/toidu_app/domain/recipe/recipe/RecipeMapper.java
@@ -0,0 +1,17 @@
+package com.vali_it.toidu_app.domain.recipe.recipe;
+
+import com.vali_it.toidu_app.service.recipe.UserRecipeRequest;
+import org.mapstruct.*;
+
+@Mapper(unmappedTargetPolicy = ReportingPolicy.IGNORE, componentModel = "spring")
+public interface RecipeMapper {
+    @Mapping(source = "usersId", target = "users.id")
+    Recipe recipeDtoToRecipe(UserRecipeRequest userRecipeRequest);
+
+    @InheritInverseConfiguration(name = "recipeDtoToRecipe")
+    UserRecipeRequest recipeToRecipeDto(Recipe recipe);
+
+    @InheritConfiguration(name = "recipeDtoToRecipe")
+    @BeanMapping(nullValuePropertyMappingStrategy = NullValuePropertyMappingStrategy.IGNORE)
+    void updateRecipeFromRecipeDto(UserRecipeRequest userRecipeRequest, @MappingTarget Recipe recipe);
+}
